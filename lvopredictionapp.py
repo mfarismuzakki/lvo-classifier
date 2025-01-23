c722fe697ddbbc6564631388d797078e83f34621
mfarismuzakki <34634822+mfarismuzakki@users.noreply.github.com>
2025-01-23 10:12:25 +0000

    hide header and footer

--- lvopredictionapp.py
+++ lvopredictionapp.py
@@ -148,9 +148,44 @@ def main():
         unsafe_allow_html=True
     )
 
+    # st.markdown(
+    # """
+    #     <style>
+    #     div[data-testid="stToolbar"] {
+    #     visibility: hidden;
+    #     height: 0%;
+    #     position: fixed;
+    #     }
+    #     div[data-testid="stDecoration"] {
+    #     visibility: hidden;
+    #     height: 0%;
+    #     position: fixed;
+    #     }
+    #     div[data-testid="stStatusWidget"] {
+    #     visibility: hidden;
+    #     height: 0%;
+    #     position: fixed;
+    #     }
+    #     #MainMenu {
+    #     visibility: hidden;
+    #     height: 0%;
+    #     }
+    #     header {
+    #     visibility: hidden;
+    #     height: 0%;
+    #     }
+    #     footer {
+    #     visibility: hidden;
+    #     height: 0%;
+    #     }
+    #     </style>
+    # """, 
+    # unsafe_allow_html=True)
+
     st.markdown(
         """
         <style>
+            header {visibility: hidden;}
             footer {visibility: hidden;}
         </style>
         """,
