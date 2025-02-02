b37cf6a7b8fe7f565486ab96a378825dea981e24
mfarismuzakki <muhammad.muzakki@majoo.id>
2025-02-02 20:47:34 +0700

    update model

--- lvopredictionapp.py
+++ lvopredictionapp.py
@@ -12,7 +12,6 @@ from keras.models import load_model
 import joblib
 import dicom2nifti
 import plotly.graph_objects as go
-import gdown
 
 # Streamlit app configuration
 st.set_page_config(
@@ -160,26 +159,24 @@ def main():
     st.sidebar.header("Upload Input Data")
 
     # Pilihan tipe model
-    model_type = st.sidebar.selectbox("Model Type", ["All Data", "Selected Column"])
-    uploaded_files = st.sidebar.file_uploader("Upload DICOM Files", accept_multiple_files=True) if model_type == "All Data" else None
+    model_type = st.sidebar.selectbox("Model Type", ["Model 1 (Acc 88%)", "Model 2 (Acc 81%)"])
+    uploaded_files = st.sidebar.file_uploader("Upload DICOM Files", accept_multiple_files=True)
 
     st.sidebar.subheader("Tabular Input Data")
 
     # Daftar kolom
     columns_all = ['jenis_kelamin', 'usia', 'dm', 'gagal_jantung', 'hipertensi', 'af', 
                    'hemiparesis', 'hemihipestesi_parestesia', 'paresis_nervus_kranialis', 
-                   'deviasi_konjugat', 'afasia', 'nihss_in', 'ddimer', 'gcs_code', 
-                   'Interpretasi_cta', 'hyperdense', 'insullar_ribbon']
-    columns_selected = ['hemiparesis', 'nihss_in', 'hyperdense', 'insullar_ribbon']
+                   'deviasi_konjugat', 'afasia', 'nihss_in', 'ddimer', 'gcs_code_(kesadaran_menurun)']
+    columns_selected = ['nihss_in']
 
     # Pilih kolom berdasarkan tipe model
-    selected_columns = columns_all if model_type == "All Data" else columns_selected
+    selected_columns = columns_all if model_type == "Model 1 (Acc 88%)" else columns_selected
 
     # Kolom boolean untuk "Ya"/"Tidak"
     boolean_columns = ['dm', 'gagal_jantung', 'hipertensi', 'af', 'hemiparesis', 
                        'hemihipestesi_parestesia', 'paresis_nervus_kranialis', 
-                       'deviasi_konjugat', 'afasia', 'hyperdense', 'insullar_ribbon', 
-                       'gcs_code', 'Interpretasi_cta']
+                       'deviasi_konjugat', 'afasia', 'gcs_code_(kesadaran_menurun)']
 
     # Input jenis_kelamin
     if 'jenis_kelamin' in selected_columns:
@@ -204,7 +201,7 @@ def main():
 
     # Pastikan kolom input sesuai dengan urutan di columns_all atau columns_selected
     final_input = []
-    for col in (columns_all if model_type == "All Data" else columns_selected):
+    for col in (columns_all if model_type == "Model 1 (Acc 88%)" else columns_selected):
         if col in tabular_input:
             final_input.append(tabular_input[col])
         else:
@@ -217,43 +214,46 @@ def main():
             random_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
             dicom_path = nifti_path = None
 
-            # Jika tipe model adalah "All Data" dan file DICOM diunggah
-            if model_type == "All Data" and uploaded_files:
-                dicom_path = save_temporary_dicom(uploaded_files, random_name)
-                if dicom_path:
-                    nifti_path = convert_dicom_to_nifti(random_name)
-                if nifti_path:
-                    img = nib.load(nifti_path)
-                    image_data = img.get_fdata()
+            dicom_path = save_temporary_dicom(uploaded_files, random_name)
+            if dicom_path:
+                nifti_path = convert_dicom_to_nifti(random_name)
+            if nifti_path:
+                img = nib.load(nifti_path)
+                image_data = img.get_fdata()
 
-                    if not os.path.exists('temporary_ct_data/slices'):
-                        os.makedirs('temporary_ct_data/slices')
+                if not os.path.exists('temporary_ct_data/slices'):
+                    os.makedirs('temporary_ct_data/slices')
 
-                    # Simpan slice MCA dan insula
-                    save_slices(image_data, f'temporary_ct_data/slices/mca_{random_name}.png', 'mca', img)
-                    save_slices(image_data, f'temporary_ct_data/slices/insula_{random_name}.png', 'insula', img)
+                # Simpan slice MCA dan insula
+                save_slices(image_data, f'temporary_ct_data/slices/mca_{random_name}.png', 'mca', img)
+                save_slices(image_data, f'temporary_ct_data/slices/insula_{random_name}.png', 'insula', img)
 
-                    st.subheader("Generated Slices")
-                    col1, col2 = st.columns(2)
-                    with col1:
-                        st.image(f'temporary_ct_data/slices/mca_{random_name}.png', caption="MCA Slice", use_container_width=True)
-                    with col2:
-                        st.image(f'temporary_ct_data/slices/insula_{random_name}.png', caption="Insula Slice", use_container_width=True)
+                st.subheader("Generated Slices")
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.image(f'temporary_ct_data/slices/mca_{random_name}.png', caption="MCA Slice", use_container_width=True)
+                with col2:
+                    st.image(f'temporary_ct_data/slices/insula_{random_name}.png', caption="Insula Slice", use_container_width=True)
 
-                    mca, insula = load_slice_image(random_name)
-                    mca_tab, insula_tab = image_to_tabular(mca, insula)
+                mca, insula = load_slice_image(random_name)
+                mca_tab, insula_tab = image_to_tabular(mca, insula)
 
-                    columns_all.append('ct_mca')
-                    columns_all.append('ct_insula')
-                    final_input.append(mca_tab)
-                    final_input.append(insula_tab)
+                columns_all.append('ct_mca')
+                columns_all.append('ct_insula')
+                columns_selected.append('ct_mca')
+                columns_selected.append('ct_insula')
+                final_input.append(mca_tab)
+                final_input.append(insula_tab)
 
-                    shutil.rmtree(f'temporary_ct_data/dicom/{random_name}')
-                    os.remove(nifti_path)
+                shutil.rmtree(f'temporary_ct_data/dicom/{random_name}')
+                os.remove(nifti_path)
+            
+            print(columns_selected)
+            print(final_input)
 
             # Pilih model berdasarkan tipe
-            model_path = 'ml_model/random_forest/random_forest_all_column.sav' if model_type == "All Data" else 'ml_model/mlp/mlp_selected_column.sav'
-            compliment_data = pd.DataFrame([final_input], columns=columns_all if model_type == "All Data" else columns_selected)
+            model_path = 'ml_model/random_forest/random_forest_all_column.sav' if model_type == "Model 1 (Acc 88%)" else 'ml_model/random_forest/random_forest_selected_column.sav'
+            compliment_data = pd.DataFrame([final_input], columns=columns_all if model_type == "Model 1 (Acc 88%)" else columns_selected)
             result, result_proba = classify(compliment_data, model_path)
 
             if result is not None:
