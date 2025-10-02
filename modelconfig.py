5c09e2faa59c75e62c5d59a03e4546c50a5b9ebd
mfarismuzakki <muhammad.muzakki@majoo.id>
2025-10-02 22:16:42 +0700

    add new model

--- modelconfig.py
+++ modelconfig.py
@@ -23,6 +23,32 @@ MODEL_CONFIGS = [
             'hct', 'at', 'ct_mca', 'ct_insula'
         ]
     },
+
+    {
+        "name": "Complete V2 RF (Acc 90.32%)",
+        "require_ct": True,
+        "path": "ml_model/random_forest/random_forest_2b.sav",
+        "columns": [
+            'usia', 'jenis_kelamin', 'dm', 'gagal_jantung', 'hipertensi',
+            'af', 'hemiparesis', 'hemihipestesi_parestesia',
+            'paresis_nervus_kranialis', 'deviasi_konjugat', 'afasia',
+            'gcs_code_(kesadaran_menurun)', 'nihss_in', 'ddimer_(di_atas_500)',
+            'hct', 'at', 'nlr', 'nc', 'leukosit', 'ct_mca', 'ct_insula'
+        ]
+    },
+    {
+        "name": "Complete V2 MLP (Acc 77.42%)",
+        "require_ct": True,
+        "path": "ml_model/mlp/mlp_2b.sav",
+        "columns": [
+            'usia', 'jenis_kelamin', 'dm', 'gagal_jantung', 'hipertensi',
+            'af', 'hemiparesis', 'hemihipestesi_parestesia',
+            'paresis_nervus_kranialis', 'deviasi_konjugat', 'afasia',
+            'gcs_code_(kesadaran_menurun)', 'nihss_in', 'ddimer_(di_atas_500)',
+            'hct', 'at', 'nlr', 'nc', 'leukosit', 'ct_mca', 'ct_insula'
+        ]
+    },
+
     {
         "name": "Selected Bivariat RF (Acc 87.10%)",
         "require_ct": True,
