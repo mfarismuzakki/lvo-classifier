4dad1c6ee1cbcd04eddb417e4f1f6bc20f0bc4dd
mfarismuzakki <muhammad.muzakki@majoo.id>
2025-10-21 17:14:31 +0700

    update

--- modelconfig.py
+++ modelconfig.py
@@ -1,31 +1,6 @@
 MODEL_CONFIGS = [
     {
-        "name": "Complete RF (Acc 90.32%)",
-        "require_ct": True,
-        "path": "ml_model/random_forest/random_forest_2.sav",
-        "columns": [
-            'usia', 'jenis_kelamin', 'dm', 'gagal_jantung', 'hipertensi',
-            'af', 'hemiparesis', 'hemihipestesi_parestesia',
-            'paresis_nervus_kranialis', 'deviasi_konjugat', 'afasia',
-            'gcs_code_(kesadaran_menurun)', 'nihss_in', 'ddimer_(di_atas_500)',
-            'hct', 'at', 'ct_mca', 'ct_insula'
-        ]
-    },
-    {
-        "name": "Complete MLP (Acc 80.65%)",
-        "require_ct": True,
-        "path": "ml_model/mlp/mlp_2.sav",
-        "columns": [
-            'usia', 'jenis_kelamin', 'dm', 'gagal_jantung', 'hipertensi',
-            'af', 'hemiparesis', 'hemihipestesi_parestesia',
-            'paresis_nervus_kranialis', 'deviasi_konjugat', 'afasia',
-            'gcs_code_(kesadaran_menurun)', 'nihss_in', 'ddimer_(di_atas_500)',
-            'hct', 'at', 'ct_mca', 'ct_insula'
-        ]
-    },
-
-    {
-        "name": "Complete V2 RF (Acc 90.32%)",
+        "name": "Skema 1 RF (Acc 90.32%)",
         "require_ct": True,
         "path": "ml_model/random_forest/random_forest_2b.sav",
         "columns": [
@@ -37,7 +12,7 @@ MODEL_CONFIGS = [
         ]
     },
     {
-        "name": "Complete V2 MLP (Acc 77.42%)",
+        "name": "Skema 1 MLP (Acc 77.42%)",
         "require_ct": True,
         "path": "ml_model/mlp/mlp_2b.sav",
         "columns": [
@@ -50,7 +25,7 @@ MODEL_CONFIGS = [
     },
 
     {
-        "name": "Selected Bivariat RF (Acc 87.10%)",
+        "name": "Skema 2 RF (Acc 87.10%)",
         "require_ct": True,
         "path": "ml_model/random_forest/random_forest_4.sav",
         "columns": [
@@ -60,7 +35,7 @@ MODEL_CONFIGS = [
         ]
     },
     {
-        "name": "Selected Bivariat MLP (Acc 74.19%)",
+        "name": "Skema 2 MLP (Acc 74.19%)",
         "require_ct": True,
         "path": "ml_model/mlp/mlp_4.sav",
         "columns": [
@@ -71,18 +46,7 @@ MODEL_CONFIGS = [
     },
 
     {
-        "name": "Selected Variable RF NC + NLR (Acc 83.87%)",
-        "require_ct": True,
-        "path": "ml_model/random_forest/random_forest_4b.sav",
-        "columns": [
-            'hemiparesis', 'hemihipestesi_parestesia',
-            'deviasi_konjugat', 'afasia', 'gcs_code_(kesadaran_menurun)', 'nihss_in',
-            'nlr', 'nc', 'ct_mca', 'ct_insula'
-        ]
-    },
-
-    {
-        "name": "Selected Variable RF NLR (Acc 83.87%)",
+        "name": "Skema 3 RF (Acc 83.87%)",
         "require_ct": True,
         "path": "ml_model/random_forest/random_forest_4c.sav",
         "columns": [
@@ -90,47 +54,5 @@ MODEL_CONFIGS = [
             'deviasi_konjugat', 'afasia', 'gcs_code_(kesadaran_menurun)', 'nihss_in',
             'nlr', 'ct_mca', 'ct_insula'
         ]
-    },
-
-
-    {
-        "name": "Bivariate Tanpa D-dimer RF (Acc 80.65%)",
-        "require_ct": True,
-        "path": "ml_model/random_forest/random_forest_7b.sav",
-        "columns": [
-            'hemiparesis', 'hemihipestesi_parestesia',
-            'deviasi_konjugat', 'afasia', 'gcs_code_(kesadaran_menurun)', 'nihss_in',
-            'ct_mca', 'ct_insula'
-        ]
-    },
-
-    {
-        "name": "Bivariate Tanpa D-dimer MLP (Acc 83.87%)",
-        "require_ct": True,
-        "path": "ml_model/mlp/mlp_7b.sav",
-        "columns": [
-            'hemiparesis', 'hemihipestesi_parestesia',
-            'deviasi_konjugat', 'afasia', 'gcs_code_(kesadaran_menurun)', 'nihss_in',
-            'ct_mca', 'ct_insula'
-        ]
-    },
-
-    {
-        "name": "Pre Hospital RF (Acc 80.65%)",
-        "require_ct": False,
-        "path": "ml_model/random_forest/random_forest_8b.sav",
-        "columns": [
-            'hemiparesis', 'hemihipestesi_parestesia',
-            'deviasi_konjugat', 'afasia', 'gcs_code_(kesadaran_menurun)'
-        ]
-    },
-    {
-        "name": "Pre Hospital MLP (Acc 80.65%)",
-        "require_ct": False,
-        "path": "ml_model/mlp/mlp_8b.sav",
-        "columns": [
-            'hemiparesis', 'hemihipestesi_parestesia',
-            'deviasi_konjugat', 'afasia', 'gcs_code_(kesadaran_menurun)'
-        ]
-    },
+    }
 ]
---
+++ modelconfig_backup.py
@@ -0,0 +1,136 @@
+MODEL_CONFIGS = [
+    {
+        "name": "Complete RF (Acc 90.32%)",
+        "require_ct": True,
+        "path": "ml_model/random_forest/random_forest_2.sav",
+        "columns": [
+            'usia', 'jenis_kelamin', 'dm', 'gagal_jantung', 'hipertensi',
+            'af', 'hemiparesis', 'hemihipestesi_parestesia',
+            'paresis_nervus_kranialis', 'deviasi_konjugat', 'afasia',
+            'gcs_code_(kesadaran_menurun)', 'nihss_in', 'ddimer_(di_atas_500)',
+            'hct', 'at', 'ct_mca', 'ct_insula'
+        ]
+    },
+    {
+        "name": "Complete MLP (Acc 80.65%)",
+        "require_ct": True,
+        "path": "ml_model/mlp/mlp_2.sav",
+        "columns": [
+            'usia', 'jenis_kelamin', 'dm', 'gagal_jantung', 'hipertensi',
+            'af', 'hemiparesis', 'hemihipestesi_parestesia',
+            'paresis_nervus_kranialis', 'deviasi_konjugat', 'afasia',
+            'gcs_code_(kesadaran_menurun)', 'nihss_in', 'ddimer_(di_atas_500)',
+            'hct', 'at', 'ct_mca', 'ct_insula'
+        ]
+    },
+
+    {
+        "name": "Complete V2 RF (Acc 90.32%)",
+        "require_ct": True,
+        "path": "ml_model/random_forest/random_forest_2b.sav",
+        "columns": [
+            'usia', 'jenis_kelamin', 'dm', 'gagal_jantung', 'hipertensi',
+            'af', 'hemiparesis', 'hemihipestesi_parestesia',
+            'paresis_nervus_kranialis', 'deviasi_konjugat', 'afasia',
+            'gcs_code_(kesadaran_menurun)', 'nihss_in', 'ddimer_(di_atas_500)',
+            'hct', 'at', 'nlr', 'nc', 'leukosit', 'ct_mca', 'ct_insula'
+        ]
+    },
+    {
+        "name": "Complete V2 MLP (Acc 77.42%)",
+        "require_ct": True,
+        "path": "ml_model/mlp/mlp_2b.sav",
+        "columns": [
+            'usia', 'jenis_kelamin', 'dm', 'gagal_jantung', 'hipertensi',
+            'af', 'hemiparesis', 'hemihipestesi_parestesia',
+            'paresis_nervus_kranialis', 'deviasi_konjugat', 'afasia',
+            'gcs_code_(kesadaran_menurun)', 'nihss_in', 'ddimer_(di_atas_500)',
+            'hct', 'at', 'nlr', 'nc', 'leukosit', 'ct_mca', 'ct_insula'
+        ]
+    },
+
+    {
+        "name": "Selected Bivariat RF (Acc 87.10%)",
+        "require_ct": True,
+        "path": "ml_model/random_forest/random_forest_4.sav",
+        "columns": [
+            'hemiparesis', 'hemihipestesi_parestesia', 'deviasi_konjugat',
+            'afasia', 'gcs_code_(kesadaran_menurun)', 'nihss_in',
+            'ddimer_(di_atas_500)', 'ct_mca', 'ct_insula'
+        ]
+    },
+    {
+        "name": "Selected Bivariat MLP (Acc 74.19%)",
+        "require_ct": True,
+        "path": "ml_model/mlp/mlp_4.sav",
+        "columns": [
+            'hemiparesis', 'hemihipestesi_parestesia', 'deviasi_konjugat',
+            'afasia', 'gcs_code_(kesadaran_menurun)', 'nihss_in',
+            'neutrofil', 'nlr', 'ct_mca', 'ct_insula'
+        ]
+    },
+
+    {
+        "name": "Selected Variable RF NC + NLR (Acc 83.87%)",
+        "require_ct": True,
+        "path": "ml_model/random_forest/random_forest_4b.sav",
+        "columns": [
+            'hemiparesis', 'hemihipestesi_parestesia',
+            'deviasi_konjugat', 'afasia', 'gcs_code_(kesadaran_menurun)', 'nihss_in',
+            'nlr', 'nc', 'ct_mca', 'ct_insula'
+        ]
+    },
+
+    {
+        "name": "Selected Variable RF NLR (Acc 83.87%)",
+        "require_ct": True,
+        "path": "ml_model/random_forest/random_forest_4c.sav",
+        "columns": [
+            'hemiparesis', 'hemihipestesi_parestesia',
+            'deviasi_konjugat', 'afasia', 'gcs_code_(kesadaran_menurun)', 'nihss_in',
+            'nlr', 'ct_mca', 'ct_insula'
+        ]
+    },
+
+
+    {
+        "name": "Bivariate Tanpa D-dimer RF (Acc 80.65%)",
+        "require_ct": True,
+        "path": "ml_model/random_forest/random_forest_7b.sav",
+        "columns": [
+            'hemiparesis', 'hemihipestesi_parestesia',
+            'deviasi_konjugat', 'afasia', 'gcs_code_(kesadaran_menurun)', 'nihss_in',
+            'ct_mca', 'ct_insula'
+        ]
+    },
+
+    {
+        "name": "Bivariate Tanpa D-dimer MLP (Acc 83.87%)",
+        "require_ct": True,
+        "path": "ml_model/mlp/mlp_7b.sav",
+        "columns": [
+            'hemiparesis', 'hemihipestesi_parestesia',
+            'deviasi_konjugat', 'afasia', 'gcs_code_(kesadaran_menurun)', 'nihss_in',
+            'ct_mca', 'ct_insula'
+        ]
+    },
+
+    {
+        "name": "Pre Hospital RF (Acc 80.65%)",
+        "require_ct": False,
+        "path": "ml_model/random_forest/random_forest_8b.sav",
+        "columns": [
+            'hemiparesis', 'hemihipestesi_parestesia',
+            'deviasi_konjugat', 'afasia', 'gcs_code_(kesadaran_menurun)'
+        ]
+    },
+    {
+        "name": "Pre Hospital MLP (Acc 80.65%)",
+        "require_ct": False,
+        "path": "ml_model/mlp/mlp_8b.sav",
+        "columns": [
+            'hemiparesis', 'hemihipestesi_parestesia',
+            'deviasi_konjugat', 'afasia', 'gcs_code_(kesadaran_menurun)'
+        ]
+    },
+]
